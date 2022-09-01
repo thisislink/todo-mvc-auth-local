5cd908e065034ef9280861bb67f9b666c6f57161
daphnelink@gmail.com <daphnelink@gmail.com>
2022-08-31 18:38:16 -0700

    updated auth.js and signup.js
    updated auth.js to persist data from form input submissions. updated signup.js to accept saved data.

--- controllers/auth.js
+++ controllers/auth.js
@@ -48,40 +48,72 @@ const User = require('../models/User')
   }
   
   exports.getSignup = (req, res) => {
-    if (req.user) {
-      return res.redirect('/todos')
-    }
-    res.render('signup', {
-      title: 'Create Account'
-    })
-  }
-  
-  exports.postSignup = (req, res, next) => {
-    const validationErrors = []
-    if (!validator.isEmail(req.body.email)) validationErrors.push({ msg: 'Please enter a valid email address.' })
-    if (!validator.isLength(req.body.password, { min: 8 })) validationErrors.push({ msg: 'Password must be at least 8 characters long' })
-    if (req.body.password !== req.body.confirmPassword) validationErrors.push({ msg: 'Passwords do not match' })
-  
-    if (validationErrors.length) {
-      req.flash('errors', validationErrors)
-      return res.redirect('../signup')
-    }
-    req.body.email = validator.normalizeEmail(req.body.email, { gmail_remove_dots: false })
-  
     const user = new User({
       userName: req.body.userName,
       email: req.body.email,
       password: req.body.password
     })
   
+    if (req.user) {
+      console.log(user.email)
+      return res.redirect('/todos')
+    }
+    res.render('signup', {
+      title: 'Create Account',
+      form: {
+        userName: user.userName,
+        email: user.email,
+        password: user.password
+      }
+    })
+  }
+  
+  exports.postSignup = (req, res, next) => {
+    const validationErrors = []
+
+    if (!validator.isEmail(req.body.email)) {
+      validationErrors.push({ msg: 'Please enter a valid email address.' })
+    }
+    if (!validator.isLength(req.body.password, { min: 8 })) {
+      validationErrors.push({ msg: 'Password must be at least 8 characters long' })
+    }
+    if (req.body.password !== req.body.confirmPassword) {
+      validationErrors.push({ msg: 'Passwords do not match' })
+    }
+
+    const user = new User({
+      userName: req.body.userName,
+      email: req.body.email,
+      password: req.body.password
+    })
+  
+    if (validationErrors.length) {    
+      return res.render('signup', {
+        errors: req.flash('errors', validationErrors),
+        form: {
+          userName: user.userName,
+          email: user.email,
+          password: user.password
+        }
+      })
+    }
+    req.body.email = validator.normalizeEmail(req.body.email, { gmail_remove_dots: false })
+  
     User.findOne({$or: [
       {email: req.body.email},
       {userName: req.body.userName}
     ]}, (err, existingUser) => {
       if (err) { return next(err) }
+      let tempEmail = req.body.email, tempUsername = req.body.userName, tempPassword = req.body.password
       if (existingUser) {
         req.flash('errors', { msg: 'Account with that email address or username already exists.' })
-        return res.redirect('../signup')
+        return res.render('signup', {
+          form: {
+            userName: tempUsername,
+            email: tempEmail,
+            password: tempPassword
+          }
+        })
       }
       user.save((err) => {
         if (err) { return next(err) }
